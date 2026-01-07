ed73c78f2b8649bef3d51312ae81b74ec72dbb4d
Aurelia <git@acuteaura.net>
2026-01-07 14:52:21 +0100

    refactor: update versions, refactor build meta

--- package.nix
+++ package.nix
@@ -40,9 +40,9 @@
   mbedtls,
   xbyak,
   zydis,
-}:
-let
-  inherit (qt6)
+}: let
+  inherit
+    (qt6)
     qtbase
     qtmultimedia
     qtwayland
@@ -51,22 +51,6 @@ let
     qtwebengine
     qt5compat
     ;
-
-  compat-list = stdenv.mkDerivation {
-    pname = "yuzu-compatibility-list";
-    version = "unstable-2024-02-26";
-
-    src = fetchFromGitHub {
-      owner = "flathub";
-      repo = "org.yuzu_emu.yuzu";
-      rev = "9c2032a3c7e64772a8112b77ed8b660242172068";
-      hash = "sha256-ITh/W4vfC9w9t+TJnPeTZwWifnhTNKX54JSSdpgaoBk=";
-    };
-
-    buildCommand = ''
-      cp $src/compatibility_list.json $out
-    '';
-  };
 
   quazip = stdenv.mkDerivation {
     pname = "quazip";
@@ -149,20 +133,13 @@ let
     ];
   };
 
-  nx_tzdb = stdenv.mkDerivation (finalAttrs: {
-    pname = "nx_tzdb";
-    version = "250725";
-    src = fetchurl {
-      url = "https://git.crueter.xyz/misc/tzdb_to_nx/releases/download/${finalAttrs.version}/${finalAttrs.version}.zip";
-      hash = "sha256-xYT/3Uzy9VGh0DaK5ouhOsCqDwu3uxkMSp++ZIL0Gcs=";
+  nx_tzdb = let
+    version = "121125";
+  in
+    builtins.fetchTarball {
+      url = "https://git.crueter.xyz/misc/tzdb_to_nx/releases/download/${version}/${version}.tar.gz";
+      sha256 = "sha256-6+qt4yzisNx8cAOrWVS+g/GCeTD37iejQN06Ij6OMxU=";
     };
-
-    nativeBuildInputs = [ unzip ];
-    buildCommand = ''
-      unzip $src -d $out
-    '';
-
-  });
 
   xbyak_new = xbyak.overrideAttrs (oldAttrs: {
     version = "7.22";
@@ -175,153 +152,159 @@ let
   });
 
   frozen = stdenv.mkDerivation (finalAttrs: rec {
-  pname = "frozen";
-  version = "61dce5ae18ca59931e27675c468e64118aba8744";
-  src = fetchFromGitHub {
-    owner = "serge-sans-paille";
-    repo = "frozen";
-    rev = "${version}";
-    hash = "sha256-zIczBSRDWjX9hcmYWYkbWY3NAAQwQtKhMTeHlYp4BKk=";
-  };
+    pname = "frozen";
+    version = "61dce5ae18ca59931e27675c468e64118aba8744";
+    src = fetchFromGitHub {
+      owner = "serge-sans-paille";
+      repo = "frozen";
+      rev = "${version}";
+      hash = "sha256-zIczBSRDWjX9hcmYWYkbWY3NAAQwQtKhMTeHlYp4BKk=";
+    };
 
-  nativeBuildInputs = [
-    cmake
-  ];
-});
-
+    nativeBuildInputs = [
+      cmake
+    ];
+  });
 in
-stdenv.mkDerivation (finalAttrs: {
-  pname = "eden";
-  version = "master";
-  src = fetchFromGitea {
-    domain = "git.eden-emu.dev";
-    owner = "eden-emu";
-    repo = "eden";
-    rev = "7751f86c1b867f7179b3b561ab8b001873c5d381";
-    hash = "sha256-THypvgdy3R7KyjeBLuO8+EVMDEIMafwXk0YVToJ2Ols=";
-    fetchSubmodules = true;
-  };
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "eden";
+    version = "0.0.4";
+    src = builtins.fetchTree {
+      type = "git";
+      url = "https://git.eden-emu.dev/eden-emu/eden.git";
+      rev = "f0fe283038c06f1911abc8ac6de2ccdd66cfb270";
+      submodules = true;
+    };
 
-  nativeBuildInputs = [
-    cmake
-    glslang
-    pkg-config
-    python3
-    qttools
-    wrapQtAppsHook
-    mcl
-    frozen
-  ];
+    nativeBuildInputs = [
+      cmake
+      glslang
+      pkg-config
+      python3
+      qttools
+      wrapQtAppsHook
+      mcl
+      frozen
+    ];
 
-  buildInputs = lib.optional stdenv.hostPlatform.isAarch64 oaknut ++ [
-    vulkan-headers
+    buildInputs =
+      lib.optional stdenv.hostPlatform.isAarch64 oaknut
+      ++ [
+        vulkan-headers
 
-    qt5compat
-    discord-rpc
-    mcl
-    boost
-    nx_tzdb
-    cpp-jwt
-    cubeb
-    enet
-    ffmpeg-headless
-    fmt_11
-    gamemode
-    httplib
-    openssl
-    libopus
-    libusb1
-    lz4
-    mbedtls
-    nlohmann_json
-    qtbase
-    qtmultimedia
-    qtwayland
-    qtwebengine
-    SDL2
-    quazip
-    simpleini
-    spirv-tools
-    spirv-headers
-    sirit
-    stb
-    unordered_dense
-    vulkan-memory-allocator
-    vulkan-utility-libraries
-    xbyak_new
-    zlib
-    zstd
-    zydis
-  ];
+        qt5compat
+        discord-rpc
+        mcl
+        boost
+        nx_tzdb
+        cpp-jwt
+        cubeb
+        enet
+        ffmpeg-headless
+        fmt_11
+        gamemode
+        httplib
+        openssl
+        libopus
+        libusb1
+        lz4
+        mbedtls
+        nlohmann_json
+        qtbase
+        qtmultimedia
+        qtwayland
+        qtwebengine
+        SDL2
+        quazip
+        simpleini
+        spirv-tools
+        spirv-headers
+        sirit
+        stb
+        unordered_dense
+        vulkan-memory-allocator
+        vulkan-utility-libraries
+        xbyak_new
+        zlib
+        zstd
+        zydis
+      ];
 
-  # dontFixCmake = true;
+    # dontFixCmake = true;
 
-  __structuredAttrs = true;
-  cmakeFlags = [
-    # actually has a noticeable performance impact
-    (lib.cmakeBool "YUZU_ENABLE_LTO" true)
-    (lib.cmakeBool "YUZU_TESTS" false)
-    (lib.cmakeBool "DYNARMIC_TESTS" false)
+    __structuredAttrs = true;
+    cmakeFlags = [
+      # actually has a noticeable performance impact
+      (lib.cmakeBool "YUZU_ENABLE_LTO" true)
+      (lib.cmakeBool "YUZU_TESTS" false)
+      (lib.cmakeBool "DYNARMIC_TESTS" false)
 
-    (lib.cmakeBool "ENABLE_QT6" true)
-    (lib.cmakeBool "ENABLE_QT_TRANSLATION" true)
-    (lib.cmakeBool "ENABLE_OPENSSL" true)
+      (lib.cmakeBool "ENABLE_QT6" true)
+      (lib.cmakeBool "ENABLE_QT_TRANSLATION" true)
+      (lib.cmakeBool "ENABLE_OPENSSL" true)
 
-    # use system libraries
-    # NB: "external" here means "from the externals/ directory in the source",
-    # so "false" means "use system"
-    (lib.cmakeBool "YUZU_USE_EXTERNAL_SDL2" false)
-    (lib.cmakeBool "YUZU_USE_EXTERNAL_VULKAN_HEADERS" false)
-    (lib.cmakeBool "YUZU_USE_EXTERNAL_VULKAN_UTILITY_LIBRARIES" false)
-    (lib.cmakeBool "YUZU_USE_EXTERNAL_VULKAN_SPIRV_TOOLS" false)
-    (lib.cmakeBool "YUZU_USE_CPM" false)
-    (lib.cmakeBool "CPMUTIL_FORCE_SYSTEM" true)
+      # use system libraries
+      # NB: "external" here means "from the externals/ directory in the source",
+      # so "false" means "use system"
+      (lib.cmakeBool "YUZU_USE_EXTERNAL_SDL2" false)
+      (lib.cmakeBool "YUZU_USE_EXTERNAL_VULKAN_HEADERS" false)
+      (lib.cmakeBool "YUZU_USE_EXTERNAL_VULKAN_UTILITY_LIBRARIES" false)
+      (lib.cmakeBool "YUZU_USE_EXTERNAL_VULKAN_SPIRV_TOOLS" false)
+      (lib.cmakeBool "YUZU_USE_CPM" false)
+      (lib.cmakeBool "CPMUTIL_FORCE_SYSTEM" true)
 
-    # nx_tzdb
-    (lib.cmakeFeature "YUZU_TZDB_PATH" "${nx_tzdb}")
+      # nx_tzdb
+      (lib.cmakeFeature "YUZU_TZDB_PATH" "${nx_tzdb}")
 
-    # don't check for missing submodules
-    (lib.cmakeBool "YUZU_CHECK_SUBMODULES" false)
+      # don't check for missing submodules
+      (lib.cmakeBool "YUZU_CHECK_SUBMODULES" false)
 
-    # enable some optional features
-    (lib.cmakeBool "YUZU_USE_QT_WEB_ENGINE" true)
-    (lib.cmakeBool "YUZU_USE_QT_MULTIMEDIA" true)
-    (lib.cmakeBool "USE_DISCORD_PRESENCE" true)
+      # enable some optional features
+      (lib.cmakeBool "YUZU_USE_QT_WEB_ENGINE" true)
+      (lib.cmakeBool "YUZU_USE_QT_MULTIMEDIA" true)
+      (lib.cmakeBool "USE_DISCORD_PRESENCE" true)
 
-    # We dont want to bother upstream with potentially outdated compat reports
-    (lib.cmakeBool "YUZU_ENABLE_COMPATIBILITY_REPORTING" false)
-    (lib.cmakeBool "ENABLE_COMPATIBILITY_LIST_DOWNLOAD" false) # We provide this deterministically
+      # We dont want to bother upstream with potentially outdated compat reports
+      # TODO: revisit this, old emulator versions will be floating around anyway
+      (lib.cmakeBool "YUZU_ENABLE_COMPATIBILITY_REPORTING" false)
+      (lib.cmakeBool "ENABLE_COMPATIBILITY_LIST_DOWNLOAD" true)
 
-    (lib.cmakeFeature "TITLE_BAR_FORMAT_IDLE" "eden | ${finalAttrs.version} (nixpkgs) {}")
-    (lib.cmakeFeature "TITLE_BAR_FORMAT_RUNNING" "eden | ${finalAttrs.version} (nixpkgs) | {}")
+      (lib.cmakeFeature "TITLE_BAR_FORMAT_IDLE" "eden | {} (nix)")
+      (lib.cmakeFeature "TITLE_BAR_FORMAT_RUNNING" "eden | {} (nix)")
 
-    # Dev
-    (lib.cmakeBool "SIRIT_USE_SYSTEM_SPIRV_HEADERS" true)
-    (lib.cmakeFeature "CMAKE_CXX_FLAGS" "-Wno-error -Wno-array-parameter -Wno-stringop-overflow")
-  ];
+      # Dev
+      (lib.cmakeBool "SIRIT_USE_SYSTEM_SPIRV_HEADERS" true)
+      (lib.cmakeFeature "CMAKE_CXX_FLAGS" "-Wno-error -Wno-array-parameter -Wno-stringop-overflow")
+    ];
 
-  env = {
-    NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isx86_64 "-msse4.2";
-  };
+    env = {
+      NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isx86_64 "-msse4.2";
+    };
 
-  qtWrapperArgs = [
-    "--prefix LD_LIBRARY_PATH : ${vulkan-loader}/lib"
-  ];
+    qtWrapperArgs = [
+      "--prefix LD_LIBRARY_PATH : ${vulkan-loader}/lib"
+    ];
 
-  postConfigure = ''
-    ln -sf ${compat-list} ./dist/compatibility_list/compatbility_list.json
-  '';
+    preConfigure = ''
+      # Set build date to commit date for reproducibility
+      export SOURCE_DATE_EPOCH=${toString finalAttrs.src.lastModified}
 
-  postInstall = ''
-    install -Dm44 $src/dist/72-yuzu-input.rules $out/lib/udev/rules.d/72-yuzu-input.rules
-  '';
+      # Provide version info for builds without .git
+      echo "${finalAttrs.version}" > GIT-REFSPEC
+      echo "${finalAttrs.src.rev}" > GIT-COMMIT
+      echo "${finalAttrs.version}" > GIT-TAG
+    '';
 
-  meta = {
-    description = "Nintendo Switch video game console emulator";
-    homepage = "https://eden-emu.dev/";
-    downloadPage = "https://eden-emu.dev/download";
-    changelog = "https://github.com/eden-emulator/Releases/releases";
-    mainProgram = "eden";
-    desktopFileName = "dist/dev.eden_emu.eden.desktop";
-  };
-})
+    postInstall = ''
+      install -Dm44 $src/dist/72-yuzu-input.rules $out/lib/udev/rules.d/72-yuzu-input.rules
+    '';
+
+    meta = {
+      description = "Nintendo Switch video game console emulator";
+      homepage = "https://eden-emu.dev/";
+      downloadPage = "https://eden-emu.dev/download";
+      changelog = "https://github.com/eden-emulator/Releases/releases";
+      mainProgram = "eden";
+      desktopFileName = "dist/dev.eden_emu.eden.desktop";
+    };
+  })
